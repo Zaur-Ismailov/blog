ec27dde8a46eaa7417a126f1c534e739ba629cd7
Zaur-Ismailov <ismailovzt@gmail.com>
2024-12-16 22:29:12 +0300

    Edit form

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name = 'home'),
     path("post/<int:pk>/", post, name="post"),
-    path('create_post/', create_post, name='create_post')
+    path('create_post/', create_post, name='create_post'),
+    path('edit_post/<int:pk>/', edit_post, name='edit_post' )
     
 ]
