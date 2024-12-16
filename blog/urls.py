32971f8a8e9f812ea81ca900c8e08b2298788746
Zaur-Ismailov <ismailovzt@gmail.com>
2024-12-16 18:08:01 +0300

    Post image

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("app/", include(('app.urls','app'), namespace='app')),
